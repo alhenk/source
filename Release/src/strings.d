df1caac40bd51b07e383188753ad01b425c3ba82
Alhen <alexandr.g.koryagin@gmail.com>
2012-11-19 16:27:26 +0600

    deleted object files

--- Release/src/strings.d
+++
@@ -1,3 +0,0 @@
-src/strings.d src/strings.o: ../src/strings.c ../src/strings.h
-
-../src/strings.h:
--- Release/src/test_strings.d
+++
@@ -1,4 +0,0 @@
-src/test_strings.d src/test_strings.o: ../src/test_strings.c \
- ../src/strings.h
-
-../src/strings.h:
